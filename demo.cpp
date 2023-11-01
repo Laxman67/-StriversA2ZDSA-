4065670d807222c8c781ffb131eb3cc289e6ea36
laxman67 <laxmanhenricks94@gmail.com>
2023-11-01 08:25:32 +0530

    Pattern 7 Done

--- demo.cpp
+++ demo.cpp
@@ -55,6 +55,56 @@ void print4(int n)
     }
 }
 
+void print5(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 0; j < n - i + 1; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void print6(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j < n - i + 1; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print7(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // Space
+        for (int j = 0; j < (n - i - 1); j++)
+        {
+            cout << " ";
+        }
+
+        // Star
+        for (int j = 0; j < (2 * i + 1); j++)
+        {
+            cout << "*";
+        }
+        // Space
+        for (int j = 0; j < (n - i - 1); j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int t;
@@ -65,7 +115,7 @@ int main()
 
         int n;
         cin >> n;
-        print4(n);
+        print7(n);
         cout << endl;
         /* code */
     }
