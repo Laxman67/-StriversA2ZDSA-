d36dc867c108d40fb78100fc5347d400c1006490
laxman67 <laxmanhenricks94@gmail.com>
2023-11-03 08:21:04 +0530

    pattern 11 and 12

--- demo.cpp
+++ demo.cpp
@@ -105,6 +105,94 @@ void print7(int n)
     }
 }
 
+void print8(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // Space
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+
+        // Star
+        for (int j = 0; j < 2 * n - (2 * i + 1); j++)
+        {
+            cout << "*";
+        }
+        // Space
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+// void print10(int n)
+// {
+
+//     for (int i = 1; i <= 2 * n - 1; i++)
+//     {
+//         int stars = i;
+//         if (i > n)
+//             stars = 2 * n - 1;
+
+//         for (int j = 1; j <= stars; i++)
+//         {
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+// }
+
+void print11(int n)
+{
+    int start = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+            start = 1;
+        else
+            start = 0;
+
+        for (int j = 0; j <= i; j++)
+        {
+            cout << start << " ";
+            start = 1 - start;
+        }
+        cout << endl;
+    }
+}
+void print12(int n)
+{
+    int space = 2 * (n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        // number
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+
+        for (int j = 1; j <= space; j++)
+        {
+            cout << " ";
+        }
+
+        // number
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+        space -= 2;
+    }
+}
+
 int main()
 {
     int t;
@@ -115,8 +203,7 @@ int main()
 
         int n;
         cin >> n;
-        print7(n);
-        cout << endl;
+        print12(n);
         /* code */
     }
 }
